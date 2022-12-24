04012a7e0aa0b9ef13a9047cce4f7f8f2ba0e799
Maksim1509 <maaa23@mail.ru>
2022-12-25 01:36:23 +0400

    faet: complete some tasks 03

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -314,8 +314,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const filtered = arr.filter((item) => typeof item === 'number' && item > 0);
+  return filtered.length;
 }
 
 /**
@@ -331,8 +332,23 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const map = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  return arr
+    .map((key) => [key, map[key]])
+    .sort(([, value1], [, value2]) => value1 - value2)
+    .map(([key]) => key);
 }
 
 /**
