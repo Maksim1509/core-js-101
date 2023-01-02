207222678370a3776100461050b4ead3bc6ef093
Maksim1509 <maaa23@mail.ru>
2023-01-03 03:10:16 +0400

    feat: /

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -106,8 +106,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let res;
+    for (let i = attempts; i > 0; i -= 1) {
+      try {
+        res = func();
+      } catch (e) {
+        res = e;
+      }
+    }
+    return res;
+  };
 }
 
 
