74288024a08cb538c6c8c2f65e0d69529cae2ef0
Maksim1509 <maaa23@mail.ru>
2023-01-03 02:48:42 +0400

    feat: /

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -81,8 +81,13 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let value;
+  return () => {
+    if (value) return value;
+    value = func();
+    return value;
+  };
 }
 
 
